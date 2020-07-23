2506e116588c0846fcf71ce2bfc1b239d8a2eec4
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-24 03:34:21 +0530

    [Vaidehi]ADD:implemented method with message as parameter

---
+++ MoodAnalyser/MoodAnalyser/MoodAnalyserException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    [Serializable]
+    public class MoodAnalyserException : Exception
+    {
+        public MoodAnalyserException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ MoodAnalyser/MoodAnalyser/MoodAnalyserUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserUtility
+    {
+        public string analyseMood(String message)
+        {
+            try
+            {
+                if(message.Equals("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalyserException("Please enter correct message");
+            }
+        }
+    }
+}
